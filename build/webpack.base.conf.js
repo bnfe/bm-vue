0bda19bf994cc7a871d569a87120c5317c420d50
mirror <1124596031@qq.com>
2023-02-14 14:34:34 +0800

    ✨ 删除无用配置

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -1,7 +1,6 @@
 const path = require("path");
 const utils = require("./utils");
 const config = require("../config");
-const vueLoaderConfig = require("./vue-loader.conf");
 
 function resolve(dir) {
   return path.join(__dirname, "..", dir);
@@ -42,7 +41,6 @@ module.exports = {
       {
         test: /\.vue$/,
         loader: "vue-loader",
-        options: vueLoaderConfig,
       },
       {
         test: /\.js$/,
