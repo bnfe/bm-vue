30de05493725ab30bdfabc1a8b76112ddae70fe6
mirror <1124596031@qq.com>
2023-02-14 22:28:02 +0800

    ✨ 增加入口图标

--- example/deploy.js
+++ example/deploy.js
@@ -1,12 +1,15 @@
 import 'babel-polyfill';
 import Vue from 'vue';
-import bmVue from '../release/lib/bm-vue.common';
+import bmVue, { Icon } from '../release/lib/bm-vue.common';
 import App from './app';
 import router from './router';
 import '../release/bm-vue.default.css';
 import './styles/pages/IndexPage.scss';
 
 Vue.config.productionTip = false;
+
+Icon.createSymbolIconfont('//at.alicdn.com/t/font_1340918_4p9b5skcr79.js');
+
 Vue.use(bmVue);
 /* eslint-disable no-new */
 new Vue({
